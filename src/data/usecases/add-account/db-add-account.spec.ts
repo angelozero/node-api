fb87b244542173abb027549bf16d27870845f081
Angelo Zero <angelo.akm@gmail.com>
2020-08-11 21:56:12 -0300

    test: create exception test for encrypter

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,46 @@
+import { Encrypter } from '../../protocols/encrypter'
+import { DbAddAccount } from './db-add-account'
+
+class EncrypterStub implements Encrypter {
+  async encrypt(value: string): Promise<string> {
+    return await new Promise(resolve => resolve('hasehd_password'))
+  }
+}
+
+describe('DbAddAccount Usecase', () => {
+
+  let dbAddAccount: DbAddAccount
+  let encrypterStub: EncrypterStub
+
+  beforeEach(() => {
+    encrypterStub = new EncrypterStub()
+    dbAddAccount = new DbAddAccount(encrypterStub)
+  })
+
+  test('Should call Encrypter with correct password', async () => {
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    await dbAddAccount.add(accountData)
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
+
+  test('Should throw excpetion if Encrypter throws exception', async () => {
+    const errorMessage = 'error_encrypter_exception'
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error(errorMessage))))
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const promise = dbAddAccount.add(accountData)
+    await expect(promise).rejects.toThrow('error_encrypter_exception')
+  })
+})
--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,15 +1,35 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
+
+  let emailValidatorAdapter: EmailValidatorAdapter
+
+  beforeEach(() => {
+    emailValidatorAdapter = new EmailValidatorAdapter()
+  })
+
   test('Should return false if an invalid email is provided', async () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = emailValidatorAdapter.isValid('invalid_email@email.com')
     expect(isValid).toBe(false)
   })
 
-  test('Should return true if an invalid email is provided', async () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
+  test('Should return true if an valid email is provided', async () => {
     const isValid = emailValidatorAdapter.isValid('valid_email@email.com')
-    expect(isValid).toBe(false)
+    expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with a valid email value before', async () => {
+    const anyEmail = 'any_email@email.com'
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    emailValidatorAdapter.isValid(anyEmail)
+    expect(isEmailSpy).toHaveBeenLastCalledWith(anyEmail)
   })
 })
